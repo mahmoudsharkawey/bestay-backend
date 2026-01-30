873747f974d42048c90712aad3ec505dadeda5b3
Mahmoud Gamal <ma7moudelsharkawey@gmail.com>
2026-01-31 01:31:26 +0200

    feat: add helmet

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import router from "./routes/v1/ApiRouter.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import page404Handler from "./middlewares/page404Handler.js";
+import helmet from "helmet";
 // Initialize Express app
 const app = express();
 
 // Middlewares setup
+app.use(helmet());
 app.use(express.json());
 // Route setup
 app.use("/api/v1", router);
