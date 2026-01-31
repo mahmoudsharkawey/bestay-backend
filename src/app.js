d05787c9eee987bf9cb1c0e93dce9bd51f599e82
Mahmoud Gamal <ma7moudelsharkawey@gmail.com>
2026-01-31 02:42:06 +0200

    feat: add cors middleware

--- src/app.js
+++ src/app.js
@@ -3,11 +3,18 @@ import router from "./routes/v1/ApiRouter.js";
 import globalErrorHandler from "./middlewares/globalErrorHandler.js";
 import page404Handler from "./middlewares/page404Handler.js";
 import helmet from "helmet";
+import cors from "cors";
 // Initialize Express app
 const app = express();
 
 // Middlewares setup
 app.use(helmet());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+}))
 app.use(express.json());
 // Route setup
 app.use("/api/v1", router);
